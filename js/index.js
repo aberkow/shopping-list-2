8b4dfd0df0b7c56d3d3fd25f846dc7d1defde15d
aberkow <berkowitz.clarinet@gmail.com>
2016-04-11 11:08:53 -0400

    working on mvc approach

---
+++ js/index.js
@@ -0,0 +1,36 @@
+var list = {};
+
+//store and manipulate app data
+list.Model = function(){
+  this.itemName = "";
+  this.shoppingList = [];
+};
+
+//add items to the model.
+list.Model.prototype.addToList = function(){
+
+};
+
+//remove items from the model.
+list.Model.prototype.removeFromList = function(){
+  
+};
+
+//display app data
+list.View = function(){
+
+};
+
+//take itemName from list.Model and display it as a <li> inside .main__list.
+list.View.prototype.renderItem = function(){
+
+};
+
+//link list.Model and list.View
+list.Controller = function(){
+
+};
+
+$(document).ready(function(){
+
+});
