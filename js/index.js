de9a4ffc89cb40595e260c78183f009d269da2f1
aberkow <berkowitz.clarinet@gmail.com>
2016-04-17 19:52:00 -0400

    switching to new branch to try a different approach

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var list = {};
 
 $(document).ready(function(){
   var model = new list.Model();
-  var view = new list.View("main__list", "main__form");
+  var view = new list.View("main__form", "main__list");
   var controller = new list.Controller(model, view);
   debugger;
 });
@@ -18,7 +18,7 @@ list.Model = function(){
 
 //add items to the model.
 list.Model.prototype.addToList = function(item){
-  this.itemName = item.itemName;
+  this.itemName = item;
   if (item !== undefined){
     this.shoppingList.push(item);
   }
@@ -32,11 +32,12 @@ list.Model.prototype.itemStatus = function(){
   //something here about whether the item has been checked or not and change its status.
 };
 
-list.Model.prototype.itemEdit = function(){
+list.Model.prototype.itemEdited = function(){
   //edit an item after it's been added to the list
+
 }
 
-list.Model.prototype.undoDelete = function(){
+list.Model.prototype.undoDeleteFromList = function(){
   //undo an accidental deletion from the list.
 }
 
@@ -48,43 +49,63 @@ list.Model.prototype.deleteItemFromList = function(item){
 };
 
 //display app data as a shopping list
-list.View = function(listSelector, formSelector){
+
+list.FormView = function(formSelector){
+  this.formSelector = $(formSelector);
+  this.ItemName = $(formSelector).find('input').val();
+}
+
+list.View = function(formSelector, listSelector){
   this.listSelector = $(listSelector);
   this.formSelector = $(formSelector);
-  // this.formSelector.on("submit", function(evt){
-  //   evt.preventDefault();
-  //   this._renderItemInView.bind(this)
-  //   debugger;
-  //   console.log("click");
-  // });
-  //debugger;
-  this.renderItemInView = null;
+  this.itemName = $(formSelector).find('input').val();
+  this.renderItemInView = null; //do I even need these two lines???
   this.removeItemFromView = null;
+  this.listeners = {};
   console.log("view");
 };
 
+list.View.prototype.addListener(type, callback){
+  var listeners = this.listeners[type] = this.listeners[type] || [];
+  listeners.push(callback);
+}
+
 //take itemName from list.Model and display it as a <li> inside .main__list.
-list.View.prototype._renderItemInView = function(evt, itemName){
+list.View.prototype.renderItemInView = function(itemName){//needs an evt here?
+  var itemName = this.listSelector.find('input').val();
+  var listener = this.listeners.add;
+  if (listeners){
+    listeners.forEach(function(listener){
+      listener(itemName);
+    });
+  }
   this.formSelector.on("submit", function(evt){
     evt.preventDefault();
-    this.renderItemInView.bind(this);
+    //this.renderItemInView.bind(this);
+    this.listSelector.append("<li class='main__list-item'>" + itemName + "<span class='main__list-item--edit'>" + "<button class='main__list-item--button'>" + "Edit" + "</button>" + "</span>" + "<button class='main__list-item--button'>" + "Delete" + "</button>" + "</span>" + "</li>"
+    );
     alert("click");
   });
-  this.listSelector.append("<li class='main__list-item'>" + itemName + "</li>");
+  // this.listSelector.append("<li class='main__list-item'>" + itemName + "</li>"
+  // );
 };
 
 //take item closest to selected trashcan and remove the entry from the view
-list.View.prototype._removeItemFromView = function(evt){
+list.View.prototype.removeItemFromView = function(evt){
   var item = evt.target;
   item.closest("main__list-item").remove();
+};
+
+list.View.prototype.editItem = function(evt, value){
+  //click on the button and edit the text of the item.
 };
 
 //link list.Model and list.View
 list.Controller = function(model, view){
   view.renderItemInView = model.addToList.bind(model);
   view.removeItemFromView = model.deleteItemFromList.bind(model);
-  model.addToList = view.renderItemInView.bind(view);
-  model.deleteItemFromList = view.removeItemFromView.bind(view);
+  // model.addToList = view.renderItemInView.bind(view);
+  // model.deleteItemFromList = view.removeItemFromView.bind(view);
 
   console.log("controller");
 
